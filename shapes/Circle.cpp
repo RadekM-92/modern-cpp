778530a4924ffe660c85defd1b6295ac0c0cf875
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-08-27 09:41:27 +0200

    Added PI number static_assert

--- shapes/Circle.cpp
+++ shapes/Circle.cpp
@@ -11,6 +11,8 @@ Circle::Circle(const Circle & other)
     r_ = other.getRadius();
 }
 
+static_assert(M_PI != 3.14, "M_PI is low precision");
+
 double Circle::getArea() const
 {
     return M_PI * r_ * r_;
